55c178ceeb37588c57896b843f890e864bde452b
mickeyyo55b <mickeyyo55b@gmail.com>
2024-11-06 16:32:37 +0800

    11061632

---
+++ src/tw/brad/apis/Bike.java
@@ -0,0 +1,13 @@
+package tw.brad.apis;
+
+public class Bike {
+    public double speed;
+
+    public void upSpeed(){
+        speed = speed < 1? 1: speed * 1.4;
+    }
+
+    public void downSpeed(){
+        speed = speed < 1? 0: speed*0.7;
+    }
+}
---
+++ src/tw/brad/tutor/Brad15.java
@@ -0,0 +1,10 @@
+package tw.brad.tutor;
+
+import tw.brad.apis.Bike;
+
+public class Brad15 {
+    public static  void main(String[] args){
+        Bike b1 = new Bike();
+        System.out.println(b1.speed);
+    }
+}
