5482a7a0d2f2c5fd219951b849a5afa5c7713f78
mickeyyo55b <mickeyyo55b@gmail.com>
2024-11-06 15:49:10 +0800

    11061550

--- src/tw/brad/tutor/PokerV3.java
+++ src/tw/brad/tutor/PokerV3.java
@@ -1,5 +1,7 @@
 package tw.brad.tutor;
 
+import java.util.Arrays;
+
 public class PokerV3 {
     public static void main(String[] args){
         final int nums = 52;
@@ -24,9 +26,13 @@ public class PokerV3 {
             players[i%4][i/4] = poker[i];
         }
 
+        //
+        String[] flowers = {"黑桃","紅心","方塊","梅花"};
+        String[] values = new String[]{"A ","2 ","3 ","4 ","5 ","6 ","7 ","8 ","9 ","10","J ","Q ","K "};
         for (int[] player  :players){
+            Arrays.sort(player);
             for (int card : player){
-                System.out.print(card + "  ");
+                System.out.printf("%s%s  ", flowers[card/13], values[card%13]);
             }
             System.out.println();
         }
