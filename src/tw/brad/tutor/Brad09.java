fce83f56f77f1f74cc8eb2176db548262e1f3f8e
mickeyyo55b <mickeyyo55b@gmail.com>
2024-11-06 10:09:40 +0800

    11061009

---
+++ src/tw/brad/tutor/Brad09.java
@@ -0,0 +1,22 @@
+package tw.brad.tutor;
+
+public class Brad09 {
+    public static void main(String[] args){
+        final int ROWS = 1;
+        final int COLS = 9;
+        final int START = 1;
+
+        //---------------------------------------------
+        for (int k=0; k < ROWS; k++) {
+            for (int j = 1; j <= 9; j++) {
+                for (int i = START; i < START+COLS; i++) {
+                    int newi = i + COLS*k;
+                    int r = newi * j;
+                    System.out.printf("%d x %d = %d\t", newi, j, r);
+                }
+                System.out.println();
+            }
+            System.out.println("----");
+        }
+    }
+}
