b2894430d9f6bf7c1f99a44253d6cbf00ec0ae92
User <User@B01>
2024-11-11 09:46:21 +0800

    11110946

--- src/tw/brad/tutor/Brad22.java
+++ src/tw/brad/tutor/Brad22.java
@@ -4,7 +4,11 @@ import tw.brad.apis.TWId;
 
 public class Brad22 {
     public static void main(String[] args){
-        String id = "Y123456789";
-        System.out.println(TWId.isRight(id));
+        String id = "A123456789";
+        if (TWId.isRight(id)) {
+        	System.out.println("OK");
+        }else {
+        	System.out.println("XX");
+        }
     }
 }
