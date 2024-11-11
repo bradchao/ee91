9168ae2e4f03144a7906afbccc3ff173e305e275
User <User@B01>
2024-11-11 16:29:47 +0800

    11111629

--- src/tw/brad/tutor/GuessNumber.java
+++ src/tw/brad/tutor/GuessNumber.java
@@ -1,6 +1,8 @@
 package tw.brad.tutor;
 
 import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -8,10 +10,11 @@ import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
-public class GuessNumber extends JFrame{
+public class GuessNumber extends JFrame implements ActionListener{
 	private JButton guess;
 	private JTextField input;
 	private JTextArea log;
+	private String answer;
 	
 	public GuessNumber() {
 		super("猜數字遊戲");
@@ -32,9 +35,60 @@ public class GuessNumber extends JFrame{
 		setSize(640, 480);
 		setVisible(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+		guess.addActionListener(this);
+		
+		initGame();
 	}
+	
+	private void initGame() {
+		answer = createAnswer();
+	}
+	
+	private String createAnswer() {
+        final int nums = 10;
+        int[] poker = new int[nums];
+        for (int i=0; i<poker.length; i++) poker[i] = i;
 
+        for (int i=nums-1; i>0; i--){
+            int rand = (int)(Math.random()*(i+1));
+            // poker[rand] <=> poker[i]
+            int temp = poker[rand];
+            poker[rand] = poker[i];
+            poker[i] = temp;
+        }
+        
+        StringBuffer sb = new StringBuffer();
+        for(int i=0; i<3; i++) sb.append(poker[i]);
+        
+		return sb.toString();
+	}
+	
+	private String checkAB(String g) {
+		return "1A2B";
+	}
+	
 	public static void main(String[] args) {
 		new GuessNumber();
 	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		String strInput = input.getText();
+		String result = checkAB(strInput);
+		log.append(String.format("%s => %s\n", strInput, result));
+		input.setText("");
+	}
 }
+
+
+//--------------------------------
+class MyListener implements ActionListener{
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		System.out.println("OK");
+	}
+	
+}
+
