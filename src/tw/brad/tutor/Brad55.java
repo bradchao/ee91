21375fbbb48bc830f46f4505d0d2a3c3ca92f087
User <User@B01.r1805.tciii.org.tw>
2024-11-15 15:35:21 +0800

    11151535

---
+++ src/tw/brad/tutor/Brad55.java
@@ -0,0 +1,32 @@
+package tw.brad.tutor;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.net.InetAddress;
+import java.net.Socket;
+
+public class Brad55 {
+	public static void main(String[] args) {
+		File source = new File("");
+		try {
+			BufferedInputStream bin = new BufferedInputStream(
+					new FileInputStream(source));
+			byte[] buf = new byte[(int)source.length()];
+			bin.read(buf);
+			bin.close();
+			//---------------------------------
+			Socket socket = new Socket(InetAddress.getByName("10.0.104.177"), 7777);
+			BufferedOutputStream bout = 
+				new BufferedOutputStream(socket.getOutputStream());
+			bout.write(buf);
+			bout.flush();
+			bout.close();
+			System.out.println("Send Success");
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+
+}
