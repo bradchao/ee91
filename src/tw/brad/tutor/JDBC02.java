140f56514605cb4ca8a32f66d6bb7305fc8d55df
User <User@B01.r1805.tciii.org.tw>
2024-11-19 16:30:24 +0800

    11191630

--- src/tw/brad/tutor/JDBC02.java
+++ src/tw/brad/tutor/JDBC02.java
@@ -2,6 +2,7 @@ package tw.brad.tutor;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Properties;
@@ -17,9 +18,21 @@ public class JDBC02 {
 					"jdbc:mysql://localhost/brad",prop);
 			
 			Statement stmt = conn.createStatement();
-			String sql = "";
-			stmt.execute(sql);
+			//String sql = "INSERT INTO cust (name,tel,birthday) VALUES ('mark','123','1999-01-02')";
+			//String sql = "DELETE FROM cust WHERE id = 3";
+			//String sql = "UPDATE cust SET tel = '456' WHERE id = 2";
+			//stmt.execute(sql);
+			//System.out.println("OK");
 			
+			String sql = "SELECT * FROM cust";
+			ResultSet rs = stmt.executeQuery(sql);
+			while (rs.next()) {
+				String id = rs.getString("id");
+				String name = rs.getString("name");
+				String tel = rs.getString("tel");
+				String birthday = rs.getString("birthday");
+				System.out.printf("%s : %s : %s : %s\n", id, name, tel, birthday);
+			}
 			
 		} catch (SQLException e) {
 			System.out.println(e);
